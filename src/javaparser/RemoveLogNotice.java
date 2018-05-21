15a68816cc52a16ebd1a70c2db5f52ff387120b2
edward.gao <edward.gao@logicmonitor.com>
2018-05-21 19:25:17 +0800

    add constructor methods for lognotice remove

--- src/javaparser/RemoveLogNotice.java
+++ src/javaparser/RemoveLogNotice.java
@@ -1,5 +1,6 @@
 package javaparser;
 
+import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.Range;
@@ -8,12 +9,12 @@ import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.expr.StringLiteralExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.utils.SourceRoot;
 
@@ -31,13 +32,13 @@ import java.util.stream.Collectors;
  * References:
  * http://javaparser.org/
  * https://www.javaadvent.com/2017/12/javaparser-generate-analyze-modify-java-code.html
- *
+ * <p>
  * This class implemented a method to replace some method like:
- *  LogNotice.info("aaa", "bb", "cc", [e])
- *      -> to
- *      LogMsg.info("aa, bb", "cc", [e])
- *
- *
+ * LogNotice.info("aaa", "bb", "cc", [e])
+ * -> to
+ * LogMsg.info("aa, bb", "cc", [e])
+ * <p>
+ * <p>
  * Note: this will overwrite the files.
  * Created by edward.gao on 10/26/16.
  */
@@ -46,6 +47,7 @@ public class RemoveLogNotice {
 
     private static String STARTPACKAGE = ""; //  the package name like "aaa.bbc.ccc"
     private static String SOURCE_FOLDER = ""; // the base source folder like "/code/java/src"
+
     public static void main(String[] args) throws IOException {
         if (args.length < 2) {
             System.out.println("Need at least two args:  codeSourceFolder startPackageName");
@@ -53,93 +55,96 @@ public class RemoveLogNotice {
         }
         SOURCE_FOLDER = args[0];
         STARTPACKAGE = args[1];
-
-        reInit(SOURCE_FOLDER, STARTPACKAGE);
+        init(SOURCE_FOLDER, STARTPACKAGE);
         while (!allCus.isEmpty()) {
             CompilationUnit u = allCus.pop();
             if (processOneFile(u)) {
-                reInit(SOURCE_FOLDER, STARTPACKAGE);
+                allCus.push(reCompile(u.getStorage().get().getPath()));
             }
         }
+
     }
 
     /**
-     *
      * Process a single compilation unit, that's a file too
+     *
      * @param u
-     * @return return whether we need to re-run for all files. because when a file changed, the lines and related information will be changed.
-     *          return true when we want to re-run for all files.
+     * @return return whether we need to re-run for all files. because when a file changed, the lines and related information will be
+     * changed.
+     * return true when we want to re-run for all files.
      * @throws IOException
      */
     public static boolean processOneFile(CompilationUnit u) throws IOException {
         System.out.println("File " + u.getStorage().get().getFileName());
-        List<MethodDeclaration> methods = u.findAll(MethodDeclaration.class);
-        for (MethodDeclaration m : methods) {
-            if (!m.getBody().isPresent()) {
-                continue;
-            }
-            BlockStmt block = m.getBody().get();
+        Stack<Node> processStack = new Stack<>();
 
-            Stack<Node> processStack = new Stack<>();
-            processStack.push(block);
-            while (!processStack.isEmpty()) {
-                Node curSt = processStack.pop();
-                if (curSt instanceof ExpressionStmt) {
-                    Expression expr = ((ExpressionStmt) curSt).getExpression();
-                    if (expr instanceof MethodCallExpr) {
-                        MethodCallExpr expression = (MethodCallExpr) expr;
-                        if (expression.getScope().isPresent() && expression.getScope().get().toString().equals("LogNotice")) {
+        // addd all constructor methods and normal methods to the process stack, we will process it later
+        u.findAll(ConstructorDeclaration.class)
+                .stream().map(m -> m.getBody())
+                .forEach(m -> processStack.push(m));
+        u.findAll(MethodDeclaration.class)
+                .stream().filter(m -> m.getBody().isPresent()).map(m -> m.getBody().get())
+                .forEach(m -> processStack.push(m));
 
-                            /**
-                             * here we start to replace the methods....
-                             * it's may be complex, but the main logic is:
-                             * if some arg is a StringLiteralExpr we can
-                             */
-                            NodeList<Expression> arguments = expression.getArguments();
-                            if (arguments.size() >= 3 && arguments.size() <= 4) {
-                                Expression first = arguments.get(0);
-                                Expression second = arguments.get(1);
-                                if (isValidExpr(first) && isValidExpr(second)) {
-                                    String reComposedMethod = reComposeMethod(expression);
 
-                                    Range lineRange = expression.getRange().get();
-                                    // expression = JavaParser.parseExpression(newString.toString());
-                                    reWriteCode(u.getStorage().get().getPath(), expression.toString(), reComposedMethod, lineRange);
-                                    System.out.println("The reformat string is " + reComposedMethod);
+        while (!processStack.isEmpty()) {
 
-                                    return true;
-                                }
-                                else {
-                                    if (first instanceof ObjectCreationExpr) {
-                                        NodeList<BodyDeclaration<?>> nodeList = first.asObjectCreationExpr()
-                                                .getAnonymousClassBody().get();
-                                        nodeList.stream().forEach(n -> processStack.push(n));
-                                    }
-                                    else {
-                                        throw new IllegalArgumentException("Not support this yet " + expression);
-                                    }
-                                }
+            Node curSt = processStack.pop();
+            if (curSt instanceof ExpressionStmt) {
+                Expression expr = ((ExpressionStmt) curSt).getExpression();
+                if (expr instanceof MethodCallExpr) {
+                    MethodCallExpr expression = (MethodCallExpr) expr;
+                    if (expression.getScope().isPresent() && expression.getScope().get().toString().equals("LogNotice")) {
 
+                        /**
+                         * here we start to replace the methods....
+                         * it's may be complex, but the main logic is:
+                         * if some arg is a StringLiteralExpr we can
+                         */
+                        NodeList<Expression> arguments = expression.getArguments();
+                        if (arguments.size() >= 3 && arguments.size() <= 4) {
+                            Expression first = arguments.get(0);
+                            Expression second = arguments.get(1);
+                            if (isValidExpr(first) && isValidExpr(second)) {
+                                String reComposedMethod = reComposeMethod(expression);
+
+                                Range lineRange = expression.getRange().get();
+                                // expression = JavaParser.parseExpression(newString.toString());
+                                reWriteCode(u.getStorage().get().getPath(), expression.toString(), reComposedMethod, lineRange);
+                                System.out.println("The reformat string is " + reComposedMethod);
+
+                                return true;
                             }
                             else {
-                                throw new IllegalArgumentException("The arguments is not 3 or 4 args - " + expression);
+                                if (first instanceof ObjectCreationExpr) {
+                                    NodeList<BodyDeclaration<?>> nodeList = first.asObjectCreationExpr()
+                                            .getAnonymousClassBody().get();
+                                    nodeList.stream().forEach(n -> processStack.push(n));
+                                }
+                                else {
+                                    throw new IllegalArgumentException("Not support this yet " + expression);
+                                }
                             }
 
                         }
-                    }
-                    else {
-                        if (!curSt.getChildNodes().isEmpty()) {
-                            for (Node n : curSt.getChildNodes()) {
-                                if (n instanceof Node) {
-                                    processStack.push(n);
-                                }
-                            }
+                        else {
+                            throw new IllegalArgumentException("The arguments is not 3 or 4 args - " + expression);
                         }
+
                     }
                 }
                 else {
-                    curSt.getChildNodes().stream().forEach(n -> processStack.push(n));
+                    if (!curSt.getChildNodes().isEmpty()) {
+                        for (Node n : curSt.getChildNodes()) {
+                            if (n instanceof Node) {
+                                processStack.push(n);
+                            }
+                        }
+                    }
                 }
+            }
+            else {
+                curSt.getChildNodes().stream().forEach(n -> processStack.push(n));
             }
         }
         return false;
@@ -148,6 +153,7 @@ public class RemoveLogNotice {
 
     /**
      * re-compose of the LogMsg from LogNotice method.
+     *
      * @param methodCallExpr
      * @return the re-write method string
      */
@@ -198,15 +204,21 @@ public class RemoveLogNotice {
 
     /**
      * whether the cu contains the LogNotice class import
+     *
      * @param u
      * @return
      */
     private static boolean containsOurMethod(CompilationUnit u) {
         for (ImportDeclaration id : u.getImports()) {
-            if (id.getName().asString().equals("com.santaba.common.logger.LogNotice")) {
+            if (id.getName().asString().equals("com.santaba.common.logger.LogNotice")
+                    ) {
                 return true;
             }
         }
+        if (u.toString().contains("LogNotice")) {
+            return true;
+        }
+        System.out.println("Skipped " + u.getStorage().get().getFileName());
         return false;
     }
 
@@ -215,13 +227,18 @@ public class RemoveLogNotice {
     }
 
 
+    private static CompilationUnit reCompile(Path file) throws IOException {
+        return JavaParser.parse(file);
+    }
+
     /**
-     * re init the cu sets.
+     * init the cu sets.
+     *
      * @param srcFolder
      * @param startPackage
      * @throws IOException
      */
-    private static void reInit(String srcFolder, String startPackage) throws IOException {
+    private static void init(String srcFolder, String startPackage) throws IOException {
         allCus.clear();
         ParserConfiguration conf = new ParserConfiguration();
         conf.setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_8);
@@ -240,12 +257,13 @@ public class RemoveLogNotice {
 
     /**
      * Re-write the code by
-     *  (1) if the original line found in file, just replace it.
-     *  (2) if not found in file, the file will be rewrite according to the line range infor
-     * @param file  the file which will be replaced or changed
+     * (1) if the original line found in file, just replace it.
+     * (2) if not found in file, the file will be rewrite according to the line range infor
+     *
+     * @param file         the file which will be replaced or changed
      * @param originalLine the original line (may be multiple lines) in file
-     * @param replaceWith replace the method to this...
-     * @param lineRange the original line range position
+     * @param replaceWith  replace the method to this...
+     * @param lineRange    the original line range position
      * @throws IOException
      */
     private static void reWriteCode(Path file, String originalLine, String replaceWith, Range lineRange) throws IOException {
